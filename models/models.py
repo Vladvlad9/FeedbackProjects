4576cc98bb5de5a97b224addf4548b874e12c1ae
Vlad Paulechka <vlad9.ru@gmail.com>
2023-11-29 16:02:55 +0300

    install commit

--- models/models.py
+++ models/models.py
@@ -10,13 +10,26 @@ Base = declarative_base()
 intPK = Annotated[int, mapped_column(primary_key=True)]
 
 
+# class User(Base):
+#     __tablename__: str = 'users'
+#
+#     id: Mapped[intPK]
+#     user_id = Column(BigInteger, nullable=False, unique=True, index=True, comment="User ID")
+#     phone: Mapped[str] = mapped_column(nullable=False, unique=True, index=True, comment="Phone number")
+#     is_active: Mapped[bool] = mapped_column(default=True, comment="Is the user active?")
+#     created_at: Mapped[datetime] = mapped_column(default=datetime.utcnow, comment="Creation timestamp")
+#     updated_at: Mapped[datetime] = mapped_column(default=datetime.utcnow, onupdate=datetime.utcnow,
+#                                                  comment="Last update timestamp")
+
 class User(Base):
     __tablename__: str = 'users'
 
-    id: Mapped[intPK]
+    id = Column(Integer, primary_key=True)
     user_id = Column(BigInteger, nullable=False, unique=True, index=True, comment="User ID")
-    phone: Mapped[str] = mapped_column(nullable=False, unique=True, index=True, comment="Phone number")
-    is_active: Mapped[bool] = mapped_column(default=True, comment="Is the user active?")
-    created_at: Mapped[datetime] = mapped_column(default=datetime.utcnow, comment="Creation timestamp")
-    updated_at: Mapped[datetime] = mapped_column(default=datetime.utcnow, onupdate=datetime.utcnow,
-                                                 comment="Last update timestamp")
+    phone = Column(String, nullable=False, unique=True, index=True, comment="User ID")
+
+    # phone: Mapped[str] = mapped_column(nullable=False, unique=True, index=True, comment="Phone number")
+    # is_active: Mapped[bool] = mapped_column(default=True, comment="Is the user active?")
+    # created_at: Mapped[datetime] = mapped_column(default=datetime.utcnow, comment="Creation timestamp")
+    # updated_at: Mapped[datetime] = mapped_column(default=datetime.utcnow, onupdate=datetime.utcnow,
+    #                                              comment="Last update timestamp")
