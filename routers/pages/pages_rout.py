48c7a256257fc400ab460d712a7f9497e6e779ae
Vlad Paulechka <vlad9.ru@gmail.com>
2023-12-07 15:33:13 +0300

    install commit

--- routers/pages/pages_rout.py
+++ routers/pages/pages_rout.py
@@ -3,7 +3,7 @@ from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
 import httpx
 
-from bot import bot
+# from bot import bot
 from config import CONFIG
 
 router = APIRouter(
